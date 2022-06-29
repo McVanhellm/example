2f88896251ef013309e9a832b78f13f5c740eadd
McVanhellm <mcvanhellm@gmail.com>
2022-06-29 21:36:52 +0300

    models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class News (models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updatet_add = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_publiched = models.BooleanField(default=True)
